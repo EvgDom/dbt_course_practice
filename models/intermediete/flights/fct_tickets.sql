c2dbf80b31f25a27b64eceb3a35843132206ac9e
Evgeny Domanov <domanov312@mail.ru>
2025-02-23 14:01:18 +0300

    урок 5, дз - исключите перелеты сотрудников авиакомпаний с помощью seed

--- models/intermediete/flights/fct_tickets.sql
+++ models/intermediete/flights/fct_tickets.sql
@@ -5,10 +5,16 @@
 }}
 
 select
-    ticket_no,
-    book_ref,
-    passenger_id,
-    passenger_name,
-    contact_data
+    t.ticket_no,
+    t.book_ref,
+    t.passenger_id,
+    t.passenger_name,
+    t.contact_data
 from
-    {{ ref('src_flights__tickets') }}
+    {{ ref('src_flights__tickets') }} t
+left join 
+    {{ ref('airline_employees') }} a
+  on 
+    t.passenger_id = a.id 
+ where 
+    a.id is null  -- Исключние билетов внутренних сотрудников
