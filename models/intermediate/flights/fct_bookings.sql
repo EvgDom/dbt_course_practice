32f350f996bd1b795c3b934acc8c8084101b8839
Evgeny Domanov <domanov312@mail.ru>
2025-05-20 18:57:00 +0300

    Урок 12, дз - часть 1

--- models/intermediate/flights/fct_bookings.sql
+++ models/intermediate/flights/fct_bookings.sql
@@ -8,9 +8,10 @@
 }}
 select 
 {# Как сделать обработку ошибки по вводу модели #}
-{{ show_columns_relation('src_flights__bookings') }}
 --    book_ref, 
 --    book_date, 
 --    total_amount
+{{ show_columns_relation('src_flights__bookings') }}
+, {{ dbt_utils.generate_surrogate_key(['book_ref']) }} as surrogate_key
 from 
     {{ ref('src_flights__bookings') }}
--- models/intermediate/flights/fct_flights.sql
+++ models/intermediate/flights/fct_flights.sql
@@ -21,3 +21,10 @@ select
     ,{{ concat_columns([ 'flight_id', 'flight_no' ]) }} as flight_info
 from
     {{ ref('src_flights__flights') }}
+
+{% if execute %}
+{%- set status_names = dbt_utils.get_column_values(
+                          table = this,
+                          column ='status')-%}
+{%- do log("Distinct status of flights: " ~ status_names|join(", "), true) -%}
+{% endif %}
